6499dbf054db2d348889fb68df7aca40fd387495
aavezirova22 <aavezirova22@codingburgas.bg>
2025-02-22 10:18:49 +0200

    Initial commit

---
+++ server/src/main/java/org/cfhackathon/netcoders/NetcodersApplication.java
@@ -0,0 +1,13 @@
+package org.cfhackathon.netcoders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetcodersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetcodersApplication.class, args);
+	}
+
+}
